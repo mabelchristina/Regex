229329de7fca9d862d60cb6d902c7086dc0db72e
Mabel Christina <maybelchristina@gmail.com>
2021-10-08 12:45:35 +0530

    [Mabel] Add:UC1 added

---
+++ RegexDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RegexDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RegexDemonstration regexDemonstration = new RegexDemonstration();
+            regexDemonstration.FirstNameChecker("Mabel");
+        }
+    }
+}
---
+++ RegexDemo/RegexDemonstration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegexDemo
+{
+    class RegexDemonstration
+    {
+        const string FIRST_NAME = "^[A-Z]{1}[a-z]{3,}$";
+       public void FirstNameChecker(string firstname)
+        {
+            if(Regex.IsMatch(firstname,FIRST_NAME))
+            { 
+                Console.WriteLine("FirstName is validated");
+                return;
+            }
+            Console.WriteLine("FirstName is not validated");
+        }
+    }
+}
